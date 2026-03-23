000fe1441bb6d94d44913e93d57af8152db4f2b4
NwizuEmmanuel <nwizuemmanuel200@gmail.com>
2026-03-23 13:45:09 +0800

    modified some code

--- scenes/play_quiz/play_quiz.gd
+++ scenes/play_quiz/play_quiz.gd
@@ -5,35 +5,27 @@ var quiz_items: Array[QuestionItem] = []
 var total_questions = 0
 var current = 0
 var score = 0
-var  MAX_LIFE = 100.0
 var GRACE_POINT = 0.5
-var player_life = 0.0
-var boss_life = 0.0
+var player_life = 100
+var boss_life = 100
 var defeat_boss_point = 0.0
-var multiple_choice_answer = 0
-var identification_answer = ""
+var failed_questions_indexes = []
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	print(Questions)
 	quiz_file = load(QuizData.quiz_path) as Questions
 	quiz_items = quiz_file.questions
 	total_questions = quiz_items.size()
 	display_question()
-	player_life = MAX_LIFE
-	boss_life = MAX_LIFE
-	update_life()
+
 
 func set_defeat_boss_point():
 	# set defeat boss point with remaining player life after
 	# boss is defeated
-	if boss_life <= 0.0:
+	if boss_life == 0.0:
 		defeat_boss_point = player_life
+		print(defeat_boss_point)
 	%DefeatBossPoint.text = "Defeat Boss Point: %d" % defeat_boss_point
-
-func update_life():
-	%BossLifeBar.value = boss_life
-	%PlayerLifeBar.value = player_life
 
 func display_question():
 	if current < total_questions:
@@ -60,53 +52,46 @@ func display_question():
 		save_quiz_data()
 		get_tree().change_scene_to_file("res://scenes/quiz_result/quiz_result.tscn")
 
-func answer_feedback(mssg:String):
-	%QuizDialog.title = "INFO"
-	%QuizDialog.dialog_text = mssg
-	%QuizDialog.popup_centered()
 
-func check_answer():
+func check_multiple_choice_answer(ans: int):
 	var failed_quiz = {}
-	if current < total_questions:
-		var q = quiz_items[current]
-		if q.question_type == QuestionItem.QuestionType.MULTIPLE_CHOICE:
-			if q.correct_option == multiple_choice_answer:
-				score += 1
-				deal_boss_damage()
-			else:
-				failed_quiz = {"id": current, "choice": multiple_choice_answer}
-				QuizData.failed_questions.append(failed_quiz)
-				deal_player_damage()
-		elif q.question_type == QuestionItem.QuestionType.IDENTIFICATION:
-			if identification_answer.to_upper() in q.correct_answer:
-				score += 1
-				deal_boss_damage()
-			else:
-				deal_player_damage()
-				failed_quiz = {"id": current, "choice": identification_answer.to_upper()}
-				QuizData.failed_questions.append(failed_quiz)
+	var q = quiz_items[current]
+	if q.question_type == QuestionItem.QuestionType.MULTIPLE_CHOICE:
+		if q.correct_option == ans:
+			score += 1
+			deal_boss_damage()
+		else:
+			deal_player_damage()
+			failed_quiz = {"id": current, "choice": ans}
+			QuizData.failed_questions.append(failed_quiz)
+	set_defeat_boss_point()
 
-func next_question():
-	current += 1
-	display_question()
+
+func check_identification_answer(ans: String):
+	var failed_quiz = {}
+	var q = quiz_items[current]
+	if q.question_type == QuestionItem.QuestionType.IDENTIFICATION:
+		if ans.to_upper() in q.correct_answer:
+			score += 1
+			deal_boss_damage()
+		else:
+			deal_player_damage()
+			failed_quiz = {"id": current, "choice": ans.to_upper()}
+			QuizData.failed_questions.append(failed_quiz)
+	set_defeat_boss_point()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	%TimerLabel.text = "TIME: %d" % %QuizTimer.time_left
 	%ScoreLabel.text = "SCORE: %d/%d" % [score,total_questions]
-	update_life()
 	timer_label_change_color_timer_stopped()
-	set_defeat_boss_point()
-	QuizData.total_questions = total_questions
-	QuizData.score = score
-	QuizData.defeat_boss_point = defeat_boss_point
 
 
 func deal_damage() -> float:
 	var result = 0
 	if !%QuizTimer.is_stopped():
 		var time_left = %QuizTimer.time_left + 1
-		var damage_point = MAX_LIFE / (total_questions - GRACE_POINT)
+		var damage_point = 100 / (total_questions - GRACE_POINT)
 		result = damage_point + time_left
 	return result
 
@@ -118,9 +103,11 @@ func timer_label_change_color_timer_stopped():
 
 func deal_boss_damage():
 	boss_life -= deal_damage()
+	%BossLifeBar.value = boss_life
 
 func deal_player_damage():
 	player_life -= deal_damage()
+	%PlayerLifeBar.value = player_life
 
 func save_quiz_data():
 	var path = "user://data/all_player_stats.res"
@@ -133,32 +120,48 @@ func save_quiz_data():
 	
 	ResourceSaver.save(all_player_stats, path)
 
+func save_data_globals():
+	QuizData.total_questions = total_questions
+	QuizData.score = score
+	QuizData.defeat_boss_point = defeat_boss_point
+
+
 func _on_option_a_pressed() -> void:
-	multiple_choice_answer = 1
-	check_answer()
-	next_question()
+	if current < total_questions:
+		check_multiple_choice_answer(1)
+		current += 1
+		save_data_globals()
+		display_question()
 
 
 func _on_option_b_pressed() -> void:
-	multiple_choice_answer = 2
-	check_answer()
-	next_question()
+	if current < total_questions:
+		check_multiple_choice_answer(2)
+		current += 1
+		save_data_globals()
+		display_question()
 
 
 func _on_option_c_pressed() -> void:
-	multiple_choice_answer = 3
-	check_answer()
-	next_question()
+	if current < total_questions:
+		check_multiple_choice_answer(3)
+		current += 1
+		save_data_globals()
+		display_question()
 
 
 func _on_option_d_pressed() -> void:
-	multiple_choice_answer = 4
-	check_answer()
-	next_question()
+	if current < total_questions:
+		check_multiple_choice_answer(4)
+		current += 1
+		save_data_globals()
+		display_question()
 
 
 func _on_identification_answer_line_edit_text_submitted(new_text: String) -> void:
-	identification_answer = new_text
-	check_answer()
-	next_question()
-	%IdentificationAnswerLineEdit.clear()
+	if current < total_questions:
+		check_identification_answer(new_text)
+		current += 1
+		save_data_globals()
+		display_question()
+		%IdentificationAnswerLineEdit.clear()
--- scenes/quiz_result/quiz_result.gd
+++ scenes/quiz_result/quiz_result.gd
@@ -52,7 +52,7 @@ func show_quiz_result():
 
 
 func _on_go_home_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/select_quiz/select_quiz.tscn")
+	get_tree().change_scene_to_file("res://scenes/main/main.tscn")
 
 
 func _on_restart_quiz_button_pressed() -> void:
